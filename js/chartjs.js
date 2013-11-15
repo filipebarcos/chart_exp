ec06c2f291c5138ee790b358f7a9a1a2586e059a
Filipe Costa <filipebarcos@gmail.com>
2013-11-15 10:16:40 -0300

    First experiment with Chart.js

---
+++ js/chartjs.js
@@ -0,0 +1,41 @@
+var lineData = {
+  labels: ['11 Nov', '12 Nov', '13 Nov', '14 Nov', '15 Nov'],
+  datasets: [
+    {
+      fillColor: 'rgba(190, 0, 0, 0.5)',
+      strokeColor: 'rgba(256, 0, 0, 1)',
+      pointColor: 'rgba(256, 0, 0, 1)',
+      pointStrokeColor: '#fff',
+      data: [100, 93, 85, 120, 132]
+    },
+    {
+      fillColor: 'rgba(0, 0, 190, 0.5)',
+      strokeColor: 'rgba(0, 0, 256, 1)',
+      pointColor: 'rgba(0, 0, 256, 1)',
+      pointStrokeColor: '#fff',
+      data: [50, 70, 150, 123, 138]
+    }
+  ]
+};
+
+var lineContext = document.getElementById('lineChart').getContext('2d');
+var lineChart = new Chart(lineContext).Line(lineData);
+
+var barData = {
+  labels: ['11 Nov', '12 Nov', '13 Nov', '14 Nov', '15 Nov'],
+  datasets: [
+    {
+      fillColor: 'rgba(190, 0, 0, 0.5)',
+      strokeColor: 'rgba(256, 0, 0, 1)',
+      data: [100, 93, 85, 120, 132]
+    },
+    {
+      fillColor: 'rgba(0, 0, 190, 0.5)',
+      strokeColor: 'rgba(0, 0, 256, 1)',
+      data: [50, 70, 150, 123, 138]
+    }
+  ]
+};
+
+var barContext = document.getElementById('barChart').getContext('2d');
+var barChart = new Chart(barContext).Bar(barData);
